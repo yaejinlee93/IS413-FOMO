783c9830abd773bbb78832dadeb1f672920fe4f1
yaejinlee93 <yaejinlee93@gmail.com>
2018-02-01 15:44:32 -0700

    done with unit testing

--- account/tests.py
+++ account/tests.py
@@ -7,13 +7,7 @@ class UserModelTest(TestCase):
     fixtures = ['data.yaml']
 
     def setUp(self):
-        self.u1 = amod.User()
-        self.u1.first_name = 'Marge'
-        self.u1.last_name = 'Simpson'
-        self.u1.email = 'marge@simpsons.com'
-        self.u1.set_password('password')
-        # ... more fields
-        self.u1.save()
+        self.u1 = amod.User.objects.get(email='marge@simpsons.com')
 
     def test_user_create_save_load(self):
         '''Test round trip of User model data to/from database'''
@@ -22,27 +16,40 @@ class UserModelTest(TestCase):
         self.assertEquals(self.u1.first_name, u2.first_name)
         self.assertEquals(self.u1.last_name, u2.last_name)
         self.assertEquals(self.u1.email, u2.email)
+        self.assertEquals(self.u1.address, u2.address)
+        self.assertEquals(self.u1.city, u2.city)
+        self.assertEquals(self.u1.state, u2.state)
         self.assertEquals(self.u1.password, u2.password)
-        self.assertTrue(u2.check_password('password'))
 
     def test_add_groups_check_permissions(self):
         '''Add groups to a users and check permissions'''
 
-        for p in Permission.objects.all():
-            print(p.content_type.app_label+ "." + p.codename)
-
-        p1 = Permission()
-        p1.name = 'Change product price'
-        p1.codename = 'change_product_price'
-        p1.content_type = ContentType.objects.get(id=1)
-        p1.save()
-        self.u1.user_permissions.add(p1)
-
-        g1 = Group()
-        g1.name = 'Salespeople'
-        g1.save()
-        g1.permissions.add(p1)
+        g1 = Group.objects.get(id=101)
         self.u1.groups.add(g1)
+        self.assertTrue(self.u1.has_perm('account.return_product'))
 
-        #self.u1.user_permissions.add(Permission.objects.get(...))
-        #self.assertTrue(self.u1.has_perm())
+    def test_add_user_permissions_check_permissions(self):
+        '''Add permissions to users and test some permissions'''
+
+        p1 = Permission.objects.get(id=101)
+        self.u1.user_permissions.add(p1)
+        self.assertTrue(self.u1.has_perm('account.return_product'))
+
+
+    def test_password(self):
+        '''Test password with set_password() and check_password()'''
+
+        u2 = amod.User()
+        u2.set_password('password')
+        self.assertTrue(u2.check_password('password'))
+
+    def test_regular_field_changes(self):
+        ''''''
+        u2 = amod.User.objects.get(email='marge@simpsons.com')
+        self.assertEquals(self.u1.first_name, u2.first_name)
+        self.assertEquals(self.u1.last_name, u2.last_name)
+        self.assertEquals(self.u1.email, u2.email)
+        self.assertEquals(self.u1.address, u2.address)
+        self.assertEquals(self.u1.city, u2.city)
+        self.assertEquals(self.u1.state, u2.state)
+        self.assertEquals(self.u1.password, u2.password)
