c6ae8393cafd491b826a1df185c99a901f71f683
yaejinlee93 <yaejinlee93@gmail.com>
2018-02-01 12:33:55 -0700

    changes

--- account/tests.py
+++ account/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 from account import models as amod
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission, Group, ContentType
 
 class UserModelTest(TestCase):
+
+    fixtures = ['data.yaml']
 
     def setUp(self):
         self.u1 = amod.User()
@@ -16,15 +18,31 @@ class UserModelTest(TestCase):
     def test_user_create_save_load(self):
         '''Test round trip of User model data to/from database'''
 
-        u2 = amod.User.objects.get(email=u1.email)
-        self.assertEquals(u1.first_name, u2.first_name)
-        self.assertEquals(u1.last_name, u2.last_name)
-        self.assertEquals(u1.email, u2.email)
-        self.assertEquals(u1.password, u2.password)
+        u2 = amod.User.objects.get(email='marge@simpsons.com')
+        self.assertEquals(self.u1.first_name, u2.first_name)
+        self.assertEquals(self.u1.last_name, u2.last_name)
+        self.assertEquals(self.u1.email, u2.email)
+        self.assertEquals(self.u1.password, u2.password)
         self.assertTrue(u2.check_password('password'))
 
     def test_add_groups_check_permissions(self):
         '''Add groups to a users and check permissions'''
 
-        admin_group = Group.objects.get(name='admin')
-        admin_group.user_set.add(amod.User.objects.get(id=1))
+        for p in Permission.objects.all():
+            print(p.content_type.app_label+ "." + p.codename)
+
+        p1 = Permission()
+        p1.name = 'Change product price'
+        p1.codename = 'change_product_price'
+        p1.content_type = ContentType.objects.get(id=1)
+        p1.save()
+        self.u1.user_permissions.add(p1)
+
+        g1 = Group()
+        g1.name = 'Salespeople'
+        g1.save()
+        g1.permissions.add(p1)
+        self.u1.groups.add(g1)
+
+        #self.u1.user_permissions.add(Permission.objects.get(...))
+        #self.assertTrue(self.u1.has_perm())
